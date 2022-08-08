1acdecefd624d1c9e9a8f0082e4fac41429f3fb9
Mohit Nagor <108342818+Shlokyy@users.noreply.github.com>
2022-08-08 13:21:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// var cel = parseFloat(document.querySelector("#cel").value); 
+// var feh = document.getElementById("feh");
+
+// function add(){
+//    let f = (cel * 9/5) + 32;
+//    feh.value = f;
+// }
+
+// console.log(cel);
+
